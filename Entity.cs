c210b8aac45e144c235b2213e25598ca64ab653b
eduardoleocr <eduardo.castillo@bridg.com>
2022-03-03 20:53:32 -0600

    Add project files.

---
+++ Entity.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GatoyRaton
+{
+    internal class Entity
+    {
+        public string Name { get; set; }
+        public int Position {get; set; }
+
+        public List<int> UsedMoves = new List<int>();
+
+        private void MoveX(int param,string n) {
+
+            
+            if (param > 9 || param < 1) {
+                Console.WriteLine("Movimiento no valido");
+                return;
+            }
+            if (UsedMoves.Count() == 0)
+            {
+                if (!n.Equals("-") && Name.Equals("Raton")) {
+                    n = "-";
+                }
+                UsedMoves.Add(param);
+            }
+            else
+            {
+                foreach (var move in UsedMoves)
+                {
+                    if (move == param)
+                    {
+                        Console.WriteLine("Este movimiento ya fue realizado");
+                        return;
+                    }
+                }
+                UsedMoves.Add(param);
+            }
+            if (n == "-")
+            {
+                Position -= param;
+            }
+            else
+            {
+                if (Name.Equals("Raton") && (Position + param) > 30)
+                {
+                    Position -= param;
+                }
+                else
+                {
+                    Position += param;
+                }
+            }
+           
+        
+        }
+
+        public void RealizarMovimiento() {
+            Console.WriteLine("Movimiento del " + Name);
+            Console.WriteLine("Movimientos disponibles: ");
+            for (int i = 1; i < 10; i++) {
+                bool flag = true;
+                foreach (var move in UsedMoves) {
+                    if (i == move) {
+                        flag = false; 
+                        break;
+                    }
+
+                }
+                if (flag) {
+                    Console.Write(i+" ");
+                }
+                
+            }
+            Console.WriteLine("");
+                string param=Console.ReadLine();
+                if (param.Contains("-")) {
+                    MoveX(Int32.Parse(param)*-1, "-");
+                    
+                }
+            else { MoveX(Int32.Parse(param), "+"); }
+                
+            Console.WriteLine("El raton esta en la posicion " + Position);
+            Console.WriteLine("********************************************");
+                return;
+            
+        
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GatoyRaton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Entity gato = new Entity { Name="Gato", Position = 0 };
+            Entity raton = new Entity { Name = "Raton", Position = 30 };
+
+
+            for (int i = 0; i < 10; i++) {
+                raton.RealizarMovimiento();
+                if (PartidaTerminada(gato, raton)) break;
+                gato.RealizarMovimiento();
+                if (PartidaTerminada(gato, raton)) break;
+            }
+
+            Console.ReadLine();
+
+
+        }
+
+        public static bool PartidaTerminada(Entity g, Entity r) {
+            if (g.Position == r.Position) {
+                Console.WriteLine("El gato ha ganado!");
+                return true;
+            }
+            if (r.Position == 0) { 
+                Console.WriteLine("El raton ha ganado!");
+                return true;
+            }
+            if (r.UsedMoves.Count() == 9 && g.UsedMoves.Count() == 9) {
+                Console.WriteLine("El juego ha quedado en un empate!");
+                return true;
+            }
+            return false;
+
+        }
+
+
+        
+    }
+}
